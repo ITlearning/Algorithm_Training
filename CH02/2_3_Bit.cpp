fac0884533cbc2c9aab5a74080e692dde4bc7805
ITlearning <yo7504@naver.com>
2021-03-11 14:16:22 +0000

    03월 11일 최종커밋

---
+++ CH02/2_3_Bit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = -43;
+	unsigned int y = x;
+	
+	cout << x << '\n';
+	cout << y << '\n';
+	
+	// 부호가 있는 정수 -x 는 부호가 없는 정수 2(n)-x와 같다.
+}
